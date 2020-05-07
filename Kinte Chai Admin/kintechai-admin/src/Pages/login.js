d5d10993121af46835c91e9d9329fc1824d79ed4
OahidZihad <oahidzihad1@gmail.com>
2020-05-08 03:19:09 +0600

    Material Ui & Firebase

---
+++ Kinte Chai Admin/kintechai-admin/src/Pages/login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class login extends Component {
+  render() {
+    return <div>login page</div>;
+  }
+}
+
+export default login;
--- Kinte Chai Admin/kintechai-admin/src/index.js
+++ Kinte Chai Admin/kintechai-admin/src/index.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+//import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+import * as serviceWorker from "./serviceWorker";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+//import { createstore, applyMiddleware, compose } from "redux";
+//import rootReducer from "./Components/Reducers/rootReducer";
+//import blue from "@material-ui/core/colors/blue";
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createstore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FFFF00",
+    },
+  },
+});
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
